38e126cb580c050f561ae3b6f7a0d6cbcfdcb561
ankithakhade301-byte <ankithakhade301@gmail.com>
2026-06-11 11:58:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+body {
+  text-align: center;
+  font-family: Arial;
+  background-color: #f2f2f2;
+}
+h1{
+    color:darkgreen;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: green;
+  color: white;
+  border:none;
+  cursor:pointer;
+  border-radius:5px;
+}
